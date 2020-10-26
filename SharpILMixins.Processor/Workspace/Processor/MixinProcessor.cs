ee075d600baf996844b3d41c233ccd6566cd9031
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-10-26 21:25:34 +0000

    Remove useless method

--- SharpILMixins.Processor/Workspace/Processor/MixinProcessor.cs
+++ SharpILMixins.Processor/Workspace/Processor/MixinProcessor.cs
@@ -66,48 +66,11 @@ namespace SharpILMixins.Processor.Workspace.Processor
                         BaseMixinActionProcessorManager.GetProcessor(action.MixinAttribute.GetType(), Workspace);
                     processor.ProcessAction(action, action.MixinAttribute);
 
-#if DEBUG
-                    //FindLeakingAssemblyRefs(action.TargetType);
-#endif
-
                     RedirectManager.ProcessRedirects(action.TargetMethod, action.TargetMethod.Body);
                     Logger.Debug($"Finished to proccess action for \"{action.MixinMethod.FullName}\"");
                 }
 
                 Logger.Info($"Finished to process mixin {mixinRelation.MixinType.Name}");
-            }
-        }
-
-        private void FindLeakingAssemblyRefs(object obj)
-        {
-            foreach (var propertyInfo in obj.GetType().GetProperties())
-            {
-                try
-                {
-                    var value = propertyInfo.GetValue(obj);
-                    if (value is AssemblyRef asmRef && !asmRef.Name.Contains("System.Runtime"))
-                    {
-                        Debugger.Break();
-                    }
-                    else if (value is ICollection collection)
-                    {
-                        foreach (var o in collection)
-                        {
-                            if (o != null && o.GetType().FullName?.StartsWith("System.") != true && !o.Equals(obj))
-                            {
-                                FindLeakingAssemblyRefs(o);
-                            }
-                        }
-                    }
-                    else if (value != null && value.GetType().FullName?.StartsWith("System.") != true && !value.Equals(obj))
-                    {
-                        FindLeakingAssemblyRefs(value);
-                    }
-                }
-                catch (Exception e)
-                {
-                    //Logger.Debug(e);
-                }
             }
         }
 
